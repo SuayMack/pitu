e6ad4b7d598ae9dc4567aba9e0a2412c51a4ef69
SuayMack <pry_chu@hotmail.com>
2020-11-28 14:29:17 -0300

    create controller

--- backend/src/controllers/links.ts
+++ backend/src/controllers/links.ts
@@ -1,14 +1,20 @@
-function postLink() {
+import {Request, Response} from 'express';
 
-
-}
-
-function postLink() {
-
+function postLink(req: Request, res: Response) {
+    res.send('postLink');
 
 }
 
-function postLink() {
+function getLink(req: Request, res: Response) {
+    res.send('getLink');
+}
 
+function hitLink(req: Request, res: Response) {
+    res.send('hitLink');
+}
 
+export default {
+    postLink,
+    getLink,
+    hitLink
 }
