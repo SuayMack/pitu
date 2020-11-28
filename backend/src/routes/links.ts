f033e19dfb605729d53341a2d9da1a313edb4c59
SuayMack <pry_chu@hotmail.com>
2020-11-28 16:22:05 -0300

    create models

--- backend/src/routes/links.ts
+++ backend/src/routes/links.ts
@@ -1,19 +1,13 @@
 import {Router} from 'express';
+import linksController  from '../controllers/links';
 
 const router = Router();//construir nova rota
 
 //configuração de rotas
-router.post('/links', (req, res) => {//enviar dados para o servidor
-    res.send('POST');
-})
-router.get('/links/:code', (req, res) => {//passa o codigo da URL curta
-    res.send('GET');
-})
+router.post('/links', linksController.postLink);
 
-router.post('/links/:code/stats', (req, res) => {//estatisticas do link
-    res.send('GET stats');
-})
+router.get('/links/:code', linksController.getLink);
+
+router.get('/links/:code/stats', linksController.hitLink)
 
 export default router;
-
-
