8344c71abd739ada0d445f7d770c2f1d40092955
SuayMack <pry_chu@hotmail.com>
2020-11-29 01:37:16 -0300

    update backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import linksRouter from './routes/links'
+import cors from 'cors';
 
 
 const app = express();
 app.use(express.json());//usando formato json de dados
+app.use(cors());
 app.use(linksRouter);
 
 export default app;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,4 +6,4 @@ database.sync();
 console.log('Database running at 3306')
 
 app.listen(3000);
-console.log('Server running at 3000');
+console.log('Server running at 3001');
