2d113f942f1c433826fc1b281e40c43f8ef718df
SuayMack <pry_chu@hotmail.com>
2020-11-29 12:56:38 -0300

    create routes

---
+++ frontend/src/routes/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+//Rotas:
+//pitu.tk/              ->HomePage
+//pitu.tk/:code         ->RedirectPage
+//pitu.tk/:code/stats   -> StatsPage
+
+import HomePage from '../pages/HomePage';
+import RedirectPage from '../pages/RedirectPage';
+import StatsPage from '../pages/StatsPage';
+
+
+function Routes() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/" component={HomePage} />
+                <Route exact path="/:code" component={RedirectPage} />
+                <Route exact path="/:code/stats" component={StatsPage} />
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default Routes;
